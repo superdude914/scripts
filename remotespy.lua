04a1b0d5bb692eb5ce92876db29d53c5b0b86b0c
superdude914 <51861305+superdude914@users.noreply.github.com>
2019-08-07 16:11:27 -0400

    Added support for multiple roblox datatypes
    CFrame, Vector3, Vector2, Color3, UDim2, Vector3int16

--- remotespy.lua
+++ remotespy.lua
@@ -59,6 +59,8 @@ local function Parse(Object)
             end
         end
         return ("{%s}"):format(Str:sub(1, -3))
+    elseif Type == "CFrame" or Type == "Vector3" or Type == "Vector2" or Type == "UDim2" or Type == "Color3" or Type == "Vector3int16" then
+        return ("%s.new(%s)"):format(Type, tostring(Object))
     else
         return tostring(Object)
     end
